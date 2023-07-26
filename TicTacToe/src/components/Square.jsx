b6234140d4b165f327f9c9543d9a36eae89cf1ae
Sauljr144 <saulsuazojr06@gmail.com>
2023-07-25 21:09:35 -0700

    add files

---
+++ TicTacToe/src/components/Square.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Square = ({value}) => {
+
+    function handleClick(){
+        console.log('clicked')
+    }
+
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  )
+}
+
+export default Square
