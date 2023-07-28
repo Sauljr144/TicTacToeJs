13118a2b60069fee3a7979454f7bbe469f5e35f4
Sauljr144 <saulsuazojr06@gmail.com>
2023-07-27 21:08:18 -0700

    Added the winning functionality

--- TicTacToe/src/components/Board.jsx
+++ TicTacToe/src/components/Board.jsx
@@ -5,15 +5,45 @@ import Square from './Square'
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i){
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  winner ? status = `Winner ${winner}` : status = `Next player: ${(xIsNext ? 'X' : 'O')}`; 
 
   return (
     <>
+    <div className='status'>{status}</div>
     <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
