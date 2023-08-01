e8d082c43a56c7cf36530a2d49e0f4d7809b7c47
Sauljr144 <saulsuazojr06@gmail.com>
2023-07-31 20:51:21 -0700

    finished game

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Board from './components/Board'
+import Game from './components/Game'
 
 
 function App() {
 
   return (
     <>
-     <Board/>
+     <Game/>
     </>
   )
 }
